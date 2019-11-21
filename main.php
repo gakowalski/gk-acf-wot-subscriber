a3c562409171d9d5ce2a49e34bb97cdd6145ad03
Grzegorz Adam Kowalski <grzegorz.adam.kowalski@outlook.com>
2019-11-21 01:41:49 +0100

    Custom CSS and JS

--- main.php
+++ main.php
@@ -4,9 +4,9 @@ Plugin Name: GK ACF Subscriber
 Plugin URI: https://grzegorzkowalski.pl/
 Description: Wtyczka modyfikuj¹ca wyœwietlanie widoku profilu subskrybenta pod k¹tem obs³ugi pól specjalnych ACF
 Author: Grzegorz Kowalski
-Version: 1.0.0
+Version: 2.0.0
 Author URI: https://grzegorzkowalski.pl/
-License:
+License: 
 */
 
 function gk_add_is_subscriber_class( $classes ) {
@@ -31,3 +31,52 @@ function gk_add_acf() {
 }
 
 add_action( 'wp_head', 'gk_add_acf' );
+
+function gk_add_custom_admin_css() {
+  echo '
+  <style>
+    body.subscriber #adminmenumain,
+	body.subscriber #screen-meta,
+	body.subscriber #screen-meta-links,
+	body.subscriber .update-nag,
+	body.subscriber .wp-heading-inline,
+	body.subscriber .wp-header-end,
+	body.subscriber #your-profile > h2,
+	body.subscriber #your-profile > h3,
+	body.subscriber #your-profile > table,
+	body.subscriber #your-profile > span,
+	body.subscriber #wpfooter {
+		display: none;
+	}
+	
+	body.subscriber #acf-form-data + h2 {
+		display: block;
+	}
+	body.subscriber #acf-form-data + h2 + table.form-table {
+		display: table;
+	}
+  </style>';
+}
+
+function gk_add_custom_admin_js() {
+  echo "
+  <script>
+	$(function() {
+		if ($('body.subscriber #acf-form-data + h2 + table.form-table').length) {
+	  		$('#submit').val('Zapisz');
+		}
+		
+		var checkbox_limit = 24;
+		
+		$('.acf-checkbox-list input').on('change', function(e) { 
+			if ($('.acf-checkbox-list input:checked').length > checkbox_limit) { 
+				alert('Nie mo¿esz umówiæ wiêcej spotkañ ni¿ ' + checkbox_limit); 
+				this.checked = false; 
+			} 
+		});
+	});
+  </script>";
+}
+
+add_action('admin_head', 'gk_add_custom_admin_css');
+add_action('admin_head', 'gk_add_custom_admin_js');
